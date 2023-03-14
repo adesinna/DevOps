a6ac305dbc1502f47c9df5d37acc08617595722c
adesinna <aladesaea@yahoo.com>
2023-03-14 15:15:46 +0100

    how to provision servers

---
+++ 3.0  Vagrant and Linux Server/myshellscript/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This will uninstall tree"
+
+sudo apt remove tree
+
+echo "Done"
---
+++ 3.0  Vagrant and Linux Server/vms/fedora/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["vagrant-vmware-desktop",["= 3.0.1"]]],"checksum":"57b5b913d3f7e14b94a3c599680ec002ea5d01574a51da0d6bbab22db93f71dc","vagrant_version":"2.3.4"}
