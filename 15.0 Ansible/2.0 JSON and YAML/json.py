0735ed8aae90148a6fb714dc0fa7da322ead0ed6
adesinna <aladesaea@yahoo.com>
2023-05-01 11:38:12 +0100

    ansible

---
+++ 15.0 Ansible/2.0 JSON and YAML/json.py
@@ -0,0 +1,29 @@
+python_dict = {
+	'name': ['aladesae', 'adesina', 'emmanuel', 'oluwaseun'],
+	'skills': ['ml', 'devops', 'web'],
+	'age': {'govt': 12,'real': 24}
+
+}
+
+# You can convert this to JSON
+
+
+JSON_FILE = {
+	'name':
+		['aladesae',
+		 'adesina',
+		 'emmanuel',
+		 'oluwaseun'],
+	'skills':
+		['ml',
+		 'devops',
+		 'web'],
+
+	'age': {
+			'govt':
+				12,
+			'real':
+				24
+		}
+
+}
