3e7a83af9be502992d2bab8862146a483f292b26
adesinna <aladesaea@yahoo.com>
2023-06-08 17:46:21 +0100

    update to script

--- 19.0 Kubernetes/2.0 Kops for K8/script.sh
+++ 19.0 Kubernetes/2.0 Kops for K8/script.sh
@@ -34,7 +34,9 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://a
 sudo apt-get update
 sudo apt-get install -y kubectl
 
-
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
 
 kops create cluster \
   --name="${CLUSTER_NAME}" \
